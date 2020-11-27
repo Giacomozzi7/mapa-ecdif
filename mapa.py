93a03f8ef748274ff1e856c616ce1c703929ad54
Francisco Giacomozzi <44447339+DigitalDream7@users.noreply.github.com>
2020-11-27 02:57:45 -0300

    mapa js

--- mapa.py
+++ mapa.py
@@ -1,25 +1,25 @@
 import sqlite3
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__,template_folder='plantillas')
 import pandas as pd
 
-def index():  #Se define la ruta del Web Server
-    return render_template("index.html")
-
-import pandas as pd
-
+@app.route('/',methods=["GET"])
 def getCoord():
     aCoord=[]
+    aCoordMax=[]; aPm=[]
     data= pd.read_csv('uma_02.csv')
     df = pd.DataFrame(data)
     aLon= df["lon"].astype('float')
     aLat= df["lat"].astype('float')
+    aMp10=df["mp10"].astype('int')
     for i in range(0,len(aLon)):
         aCoord.append([aLat[i],aLon[i]])
+        if aMp10[i]>15:
+            aCoordMax.append([aLat[i],aLon[i]])
+            aPm.append(aMp10[i])
     
-    return aCoord
-
-df = getCoord()
+    print(aPm)
+    return render_template("index.html",aCoord=aCoord,aCoordMax=aCoordMax,aPm=aPm)
 
 
 if __name__ == "__main__":
